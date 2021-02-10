bbc332f0971f8c7aae359b64bec79194cc023e92
mahdyrahman98 <MRahman2@qa.com>
2021-02-10 15:50:01 +0000

    update to schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -11,17 +11,6 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS orderitems (
-order_item_id INT NOT NULL,
-order_id INT NOT NULL,
-item_id INT NOT NULL, 
-item_quantity INT NOT NULL,
-PRIMARY KEY (order_item_id),
-FOREIGN KEY (order_id) REFERENCES orders(order_id),
-FOREIGN KEY (item_id) REFERENCES item(item_id)
-);
-
-
 CREATE TABLE IF NOT EXISTS item (
 item_id INT NOT NULL AUTO_INCREMENT,
 item_name CHAR(50) NOT NULL,
@@ -32,11 +21,18 @@ PRIMARY KEY (item_id)
 CREATE TABLE IF NOT EXISTS orders (
 order_id INT NOT NULL AUTO_INCREMENT,
 customer_id INT NOT NULL,
-order_value DECIMAL (7, 2) NOT NULL,
-order_date DATE NOT NULL,
 PRIMARY KEY (order_id),
 FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
+
+CREATE TABLE IF NOT EXISTS orderitems (
+order_id INT NOT NULL,
+item_id INT NOT NULL, 
+item_quantity INT NOT NULL,
+FOREIGN KEY (order_id) REFERENCES orders(order_id),
+FOREIGN KEY (item_id) REFERENCES item(item_id)
+);
+
 
 
 SELECT i.item_id, i.item_name, i.price, y.order_id, y.item_quantity
